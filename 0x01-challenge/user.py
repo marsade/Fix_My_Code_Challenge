69f02103961c66ef76e0ec8db89ff1d30c6f8798
marsade <adenijifara@gmail.com>
2024-01-22 16:44:47 +0200

    Fix code

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" 
+"""
 User class
 """
+
 
 class User():
     """ Documentation """
@@ -10,21 +11,20 @@ class User():
         """ Documentation """
         self.__email = None
 
-    @email.setter
+    @property
     def email(self, value):
         """ Documentation """
         if type(value) is not str:
             raise TypeError("email must be a string")
         self.__email = value
 
-    @property
+    @email.getter
     def email(self):
         """ Documentation """
         return self.__email
-   
-    
-if __name__ == "__main__":
 
+
+if __name__ == "__main__":
     u = User()
     u.email = "john@snow.com"
     print(u.email)
