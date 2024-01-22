d9a232d1a376cd73721f61b33d51873b837ac97e
marsade <adenijifara@gmail.com>
2024-01-22 15:44:33 +0200

    Fix code

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -15,7 +15,7 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
-        return 2 * (self.width + self.height)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
